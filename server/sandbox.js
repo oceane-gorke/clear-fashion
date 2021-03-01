5c1b09278ce5492e7441eda3aa6590ef1d0468ed
oceane-gorke <74909807+oceane-gorke@users.noreply.github.com>
2021-03-01 17:55:57 +0100

    session4

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const adresseparis = require('./sources/adresseparis');
+const mudjeans = require('')
 
 async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
-
+    //const products = await dedicatedbrand.scrape(eshop);
+    const products = await adresseparis.scrape(eshop);
     console.log(products);
     console.log('done');
     process.exit(0);
@@ -19,3 +21,26 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
 const [,, eshop] = process.argv;
 
 sandbox(eshop);
+
+//in the terminal do : 
+//node sandbox.js -> log the products from eshop defined above 
+//or if you want another link : node sandbox.js "https://www.dedicatedbrand.com/en/men/t-shirts" 
+
+
+/*
+const mudjeans = require('./sources/mudjeans');
+//Mud Jeans
+echop2='https://mudjeans.eu';
+
+const [,, eshop2] = process.argv;
+
+sandbox(eshop2);
+
+
+const adresseparis = require('./sources/adresse.paris');
+//Adresse Paris
+echop3='https://adresse.paris';
+const [,, eshop3] = process.argv;
+
+sandbox(eshop3);
+*/
--- server/sources/dedicatedbrand.js
+++ server/sources/dedicatedbrand.js
@@ -44,3 +44,81 @@ module.exports.scrape = async url => {
 
   return null;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+/*
+function parseHomepage (data){
+  const $ = cheerio.load(data);
+
+  return $('.mainNavigation-link-subMenu-link')
+    .map((i, element) => {
+      const href = $(element).find('a').attr('href');
+      return `${DEDICATED_BRAND}${href}`;
+    })
+    .get();
+};
+*/
+/**
+ * Get all the products pages
+ * @param {String} url 
+ * @return {Array}
+ */
+/*
+module.exports.getPages = async (url = DEDICATED_BRAND) => {
+  const response = await axios(url);
+  const {data, status} = response;
+
+  if (status >= 200 && status < 300){
+    return parseHomepage(data);
+  }
+
+  console.error(status);
+  
+  return [];
+
+};
+
+*/
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+/*
+module.exports.scrape = async url => {
+  const response = await axios(url);
+  const {data, status} = response;
+
+  if (status >= 200 && status < 300) {
+    return parse(data);
+  }
+
+  console.error(status);
+
+  return null;
+};
+*/
